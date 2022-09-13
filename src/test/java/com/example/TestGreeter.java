f9ae905cafb8cc58de2a4495a538111ad245aaa6
root <root@ip-172-31-28-84.ec2.internal>
2022-09-13 01:36:08 +0000

    new test case added

--- src/test/java/com/example/TestGreeter.java
+++ src/test/java/com/example/TestGreeter.java
@@ -34,10 +34,10 @@ public class TestGreeter {
     assertThat(greeter.greet(someone).length(), is(greaterThan(someone.length())));
   }
 
- /*   @Test
+    @Test
   public void greetShouldIncludeGreetingMessage() {
     String someone = "World, Thanks you making me Happy";
 
     assertThat(greeter.greet(someone).length(), is(greaterThan(someone.length())));
-  } */
+  } 
 }
